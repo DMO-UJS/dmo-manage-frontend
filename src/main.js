89db40b042994103f15eeaebb9937aa22798fa39
chen <cc_traveller@163.com>
2018-11-08 12:35:14 +0800

    v0.0.1 初稿

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueResource from 'vue-resource'
 import './plugins/element.js'
 
 Vue.config.productionTip = false
-
+Vue.use(VueResource)
+/* eslint-disable */
 new Vue({
   router,
   render: h => h(App)
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,45 @@
 <template>
 	<div class="about">
 		<el-container>
-			<el-header height="150px">
-				<img src="../assets/医院.svg">
+			<el-header height="100px">
+				<img id="logo" src="../assets/医院.svg">
 				<br/>
 				镇江第四人民医院
 			</el-header>
 			<el-container>
-				<el-aside width="400px">
-					<el-button type="primary">创建本体库</el-button>
+				<el-aside width="200px" class="aside">
+					<el-button type="primary" @click="dialogVisable = true">创建本体库</el-button>
+					<el-dialog title="创建本体库" :visible.sync="dialogVisable" width="800px">
+						<el-form :model="ontologyLibrary">
+							<el-form-item label="本体库名称">
+						    <el-input v-model="ontologyLibrary.name"></el-input>
+						  </el-form-item>
+						  <el-form-item label="描述">
+						  	<el-input type="textarea" v-model="ontologyLibrary.description"></el-input>
+						  </el-form-item>
+						  <el-form-item align="left">
+						  	<el-switch
+						  		v-model="ontologyLibrary.state"
+						  		active-text="公开"
+						  		inactive-text="私有"
+						  	>
+						  	</el-switch>
+						  </el-form-item>
+						  <el-form-item label="选择现有本体文件">
+						  	<el-input type="file" class="fileItem"></el-input>
+						  </el-form-item>
+						  <el-form-item>
+						  	<el-button type="primary" @click="createOntology()">立即创建</el-button>
+						  </el-form-item>
+						</el-form>
+					</el-dialog>
 				</el-aside>
 				<el-main>
 					<el-table
-					:data="ontologyLibrary"
-					height="250"
-					stripe
-					border
+					:data="ontologyLibraryList"
+					highlight-current-row
+    			@current-change="handleCurrentChange"
+    			@row-dblclick="handleCurrentEdit"
 					style="width: 100%"
 					:deafult-sort = "{prop: 'data', order: 'descending'}">
 						<el-table-column
@@ -41,11 +65,11 @@
 						</el-table-column>
 						<el-table-column
 							label="操作"
-							width="100">
-							<template slot-scope="scope">
-								<el-button>
-									<img src="../assets/医院.svg">
-							 	</el-button>
+							align=center
+							width="200">
+							<template scope="scope">
+								<el-button icon="el-icon-edit" @click="handleEdit(scope.$index, scope.row)"></el-button>
+								<el-button icon="el-icon-delete" @click="handleDelete(scope.$index, ontologyLibrary)"></el-button>
 							</template>
 						</el-table-column>
 					</el-table>
@@ -57,11 +81,17 @@
 </template>
 
 <script>
+
 	export default {
 		name: 'about',
 		data() {
 			return {
-				ontologyLibrary: [{
+				ontologyLibrary: {
+					name: "妊娠期本体",
+					description: "妊娠期本体",
+					state: true,
+				},
+				ontologyLibraryList: [{
 					name: "妊娠期糖尿病",
 					owner: "胡惊涛",
 					date: "2018-11-05",
@@ -76,15 +106,65 @@
 					owner: "陈",
 					date: "2018-11-07",
 					state: "私有"
-				}]
+				}],
+				options: [{
+					value: "打开",
+				}, {
+					value: "下载",
+				}, {
+					value: "删除"
+				}],
+				currentRow: null,
+				value: '',
+				dialogVisable: false,
+				createUrl: 'http://192.168.1.102:5000/ontocreate'
 			}
+		},
+		methods: {
+			createOntology: function () {
+				this.dialogVisable = false
+				let message = this.ontologyLibrary
+				message.owner = "陈"
+				this.$http.post(this.createUrl, message)
+         .then((response) => {
+         		 console.log(response.data)
+         			this.programs = response.data
+         }, (response) => {
+         	alert(response)
+         })
+			},
+			handleCurrentChange : function (val) {
+				this.currentRow = val
+			},
+			handleCurrentEdit: function (row) {
+				console.log(row)
+			},
+			handleEdit: function (index, rowData) {
+				console.log(rowData)
+			},
+			handleDelete(index, rows) {
+		    rows.splice(index, 1);
+		  }
 		}
 	}
 </script>
 
-<style scoped>
-	img {
+<style>
+	.aside {
+		padding: 35px;
+	}
+
+	#logo {
 		width: 50px;
 		height: 50px;
+	}
+
+	.fileItem input {
+		border: none;
+		line-height: 100%;
+	}
+	el-table el-button {
+		border: none;
+		outline: none;
 	}
 </style>
